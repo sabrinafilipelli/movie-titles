783920fe03ddd1fb2cc0dc59411f248e194d0c8f
SabrinaGear <sabrinagear.ft@gmail.com>
2019-05-03 22:39:15 -0500

    styles added

--- README.md
+++ README.md
@@ -1,47 +1,5 @@
-# movie-titles
+# Movie Titles
 
-A repository to show that makes calls to the hackerrank movie-titles API using a slightly more realistic development environment and libraries such as Axios and React. But the logic is still the same.
+A basic solution to Hackerrank's Movie Titles challenge using dev libraries such as Axios and React. Easier, more user-friendly, but the logic is still the same.
 
-Code:
-
-import React, { Component } from "react";
-import axios from "axios";
-
-class App extends Component {
-constructor() {
-super();
-this.state = {
-movies: []
-};
-}
-
-componentDidMount() {
-axios
-.get(
-"https://jsonmock.hackerrank.com/api/movies/search/?Title=spiderman&page=1"
-)
-.then(response => {
-this.setState(() => ({ movies: response.data.data }));
-console.log(response.data.data);
-})
-.catch(err => {
-console.error(err.message);
-});
-}
-
-render() {
-console.log(this.state.movies);
-return (
-<div className="App">
-<div className="list">
-{this.state.movies.map(movie => (
-<p>{movie.Title}</p>
-))}
-</div>
-<button className="main-btn">View on Github</button>
-</div>
-);
-}
-}
-
-export default App;
+Deployed here:
--- movies/src/App.js
+++ movies/src/App.js
@@ -1,41 +1,47 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      movies: []
-    };
-  }
+function App() {
+  const [search, setSearch] = useState("");
+  const [searchedMovies, setSearchedMovies] = useState([]);
 
-  componentDidMount() {
-    axios
-      .get(
-        "https://jsonmock.hackerrank.com/api/movies/search/?Title=spiderman&page=1"
-      )
-      .then(response => {
-        this.setState(() => ({ movies: response.data.data }));
-        console.log(response.data.data);
-      })
-      .catch(err => {
-        console.error(err.message);
-      });
-  }
-
-  render() {
-    console.log(this.state.movies);
-    return (
-      <div className="App">
-        <div className="list">
-          {this.state.movies.map(movie => (
-            <p>{movie.Title}</p>
+  return (
+    <div className="App">
+      <div className="a">
+        <h1>Movie Titles II</h1>
+        <h3>A Reimagined Algorithm Test</h3>
+        <form
+          onSubmit={async e => {
+            e.preventDefault();
+            const searched = await axios.get(
+              `https://jsonmock.hackerrank.com/api/movies/search/?Title=${search}&page=1`
+            );
+            setSearchedMovies(searched.data.data);
+            setSearch("");
+          }}
+        >
+          <input
+            className="search"
+            type="text"
+            placeholder="search"
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+          />
+          <input className="main-btn" type="submit" value="magic" />
+        </form>
+        <ul className="list">
+          {searchedMovies.map(movie => (
+            <li key={movie.imdbID}>{movie.Title}</li>
           ))}
-        </div>
-        <button className="main-btn">View on Github</button>
+        </ul>
       </div>
-    );
-  }
+      <footer>
+        <a href="https://github.com/sabrinagear/movie-titles" className="code">
+          Check out the code on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
